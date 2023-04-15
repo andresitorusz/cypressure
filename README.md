1bd2f63848d00a1a8f2a42c3c7f8a70bfe34db13
&re <andresitorusz.19@gmail.com>
2023-04-15 22:56:36 +0700

    Tidy up const and update doc

--- README.md
+++ README.md
@@ -17,26 +17,12 @@
 
 
 ### Setup
-##### Prerequisites
-- Any IDE.
-- NodeJS at least v19.6.0.
-
-
-##### Steps
-1. Run `git clone https://github.com/helenanull/cypress-example.git` in your Terminal.
-2. Run `cd learning-cypress`. In learning **learning-cypress** folder, there will be several app.
-
-
-### Run tests
-1. Navigate to app you want to test, e.g. `app/petstore`.
-2. Install the **node_modules** by running `npm install`.
-3. Run `npm run cy` or `npx cypress open` to open the test UI.
-4. Run the tests.
-
+Read the setup and how to run guide in the app folder **README**.
 
 ### Misc
 ##### App list
-- [Petstore](https://petstore3.swagger.io/) 
+- [Petstore](https://petstore3.swagger.io/)
+- ...
 
 ##### IDE setup and recommended extensions
 [VS Code](https://code.visualstudio.com/download) with following extensions:
--- app/petstore/cypress/e2e/consts/store.js
+++ app/petstore/cypress/e2e/consts/store.js
@@ -1,36 +1,31 @@
-export const SUCCESSFUL_FIND = "200 OK";
-export const SUCCESSFUL_CREATE = "200 Created";
-
-export const BAD_REQUEST = "400 Bad Request";
-
-export const ORDER_NOT_FOUND = "404 Order Not Found";
-export const INVALID_ID_SUPPLIED = "400 Invalid ID Supplied";
-
-
-// URLs & Endpoints
-export const BASE_URL = Cypress.env("BASE_URL") + Cypress.env("STORE");
-export const INVENTORY_ENDPOINT = Cypress.env("INVENTORY");
-export const ORDER_ENDPOINT = Cypress.env("ORDER");
-
-// Store
-export const ORDER_INVALID_ID = "0o07";
-export const INVENTORY_SCHEMA = {
-  title: "Inventory Schema",
-  type: "object",
-  required: ["approved", "placed", "delivered"],
-  properties: {
-    approved: {
-      type: "number",
-    },
-    placed: {
-      type: "number",
-    },
-    delivered: {
-      type: "number",
+export const // Operation
+  SUCCESSFUL_FIND = "200 OK",
+  SUCCESSFUL_CREATE = "200 Created",
+  BAD_REQUEST = "400 Bad Request",
+  ORDER_NOT_FOUND = "404 Order Not Found",
+  INVALID_ID_SUPPLIED = "400 Invalid ID Supplied",
+  // URLs & Endpoints
+  BASE_URL = Cypress.env("BASE_URL") + Cypress.env("STORE"),
+  INVENTORY_ENDPOINT = Cypress.env("INVENTORY"),
+  ORDER_ENDPOINT = Cypress.env("ORDER"),
+  // Store
+  ORDER_INVALID_ID = "0o07",
+  INVENTORY_SCHEMA = {
+    title: "Inventory Schema",
+    type: "object",
+    required: [],
+    properties: {
+      approved: {
+        type: "number",
+      },
+      placed: {
+        type: "number",
+      },
+      delivered: {
+        type: "number",
+      },
     },
   },
-};
-
-// Error
-export const INPUT_ERROR =
-  "Input error: unable to convert input to io.swagger.petstore.model.Order";
+  // Error
+  INPUT_ERROR =
+    "Input error: unable to convert input to io.swagger.petstore.model.Order";
